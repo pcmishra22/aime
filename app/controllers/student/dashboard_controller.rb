7c6d16bcfae5e868123c82b7172294d886da7f79
Nilay <nilayanand@gmail.com>
2016-10-05 14:52:49 +0530

    Add sudent controller and view (Intial)

---
+++ app/controllers/student/dashboard_controller.rb
@@ -0,0 +1,7 @@
+class Student::DashboardController < Student::ApplicationController
+  layout 'student'
+
+  def index
+
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,21 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  
+
+
+  def is_student?
+    type == "Student"
+  end
+
+  def is_teacher?
+    type == "Teacher"
+  end
+
+  def is_parent?
+    type == "Parent"
+  end
+
+  def is_admin?
+    type == "Admin"
+  end
 end
