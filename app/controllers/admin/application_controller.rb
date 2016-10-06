f741f4feabb258c2db0b12689f9bf31487532b7a
Nilay <nilayanand@gmail.com>
2016-10-06 11:48:36 +0530

    Add dashboard for each sub system

---
+++ app/controllers/admin/application_controller.rb
@@ -0,0 +1,24 @@
+class Student::ApplicationController < ApplicationController
+  layout 'admin'
+
+  before_action :verify_user_auth!
+  before_action :authorize_student!
+  before_action :set_active_menu
+
+  # Override this value to specify the number of elements to display at a time
+  # on index pages. Defaults to 20.
+  def records_per_page
+    params[:per_page] || 100
+  end
+
+  private
+
+  def authorize_student!
+    render "errors/unauthorized", status: 401, layout: false unless current_user.is_admin?
+  end
+
+  def set_active_menu
+    @active_menu = params[:controller].gsub('admin/','')
+  end
+
+ end
---
+++ app/controllers/teacher/dashboard_controller.rb
@@ -0,0 +1,7 @@
+class Student::DashboardController < Student::ApplicationController
+  layout 'teacher'
+
+  def index
+
+  end
+end
