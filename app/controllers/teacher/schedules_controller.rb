b5e56becc2fc36477fbb727328d5809d62b1e4ca
Devzila <tedsolutions@TEDs-Mac-mini.local>
2016-10-18 17:03:57 +0530

    Date Changes

--- app/controllers/teacher/schedules_controller.rb
+++ app/controllers/teacher/schedules_controller.rb
@@ -1,12 +1,16 @@
 class Teacher::SchedulesController < Teacher::ApplicationController
   layout 'teacher'
-	before_action :set_courses,only:[:index,:new,:create,:edit,:update]
+  before_action :set_courses,only:[:index,:new,:create,:edit,:update]
+	before_action :validate_course,only:[:create,:update]
+
   def index
-    @schedule = Schedule.where(:course_id=>14)
+    @schedule = Schedule.where(:course_id=>params[:course_id])
   end
+
   def new  	
   	@schedule = Schedule.new
   end
+
   def create
   	@schedule = Schedule.new(schedule_params)
     @course_id = @schedule.course_id
@@ -16,9 +20,11 @@ class Teacher::SchedulesController < Teacher::ApplicationController
   		render :new
   	end
   end
+
   def edit
     @schedule = Schedule.find(params[:id])
   end
+
   def update
     @schedule = Schedule.find(params[:id])
     @course_id = @schedule.course_id
@@ -28,12 +34,23 @@ class Teacher::SchedulesController < Teacher::ApplicationController
       render :edit
     end
   end
+
   private
+
   def schedule_params
     params.require(:schedule).permit(:topic,:course_id,:startdate,:enddate,:starttime,:endtime)
   end
+
   def set_courses
     @course_id = params[:course_id]
   	@courses = Course.my_active_courses(current_user.id)
   end
+
+  def validate_course
+    @course = Course.find(params[:schedule][:course_id])
+    if(@course.teacherid != current_user.id)
+      redirect_to teacher_courses_path ,:alert=>"Something is wrong.Please try again."
+    end
+  end
+
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,5 @@
 class Course < ActiveRecord::Base
+	belongs_to :user
 	has_many :schedule
 	validates :name, :presence => true
 	def self.my_active_courses(user_id)
