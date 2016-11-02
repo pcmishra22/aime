58faf64c07e478e5cb40623f03c15a6063d67c02
Devzila <tedsolutions@TEDs-Mac-mini.local>
2016-11-02 15:56:57 +0530

    Inbox Count

--- app/models/mailbox.rb
+++ app/models/mailbox.rb
@@ -1,10 +1,10 @@
 class Mailbox < ActiveRecord::Base
 	before_create :recipient_user
 
-	validates :to_email, :presence => true , :on => :create
-	validates :end_date, :presence => true , :on => :create
-	validates :subject, :presence => true , :on => :create
-	validates :topics, :presence => true , :on => :create
+	validates :to_email, :presence => true
+	validates :end_date, :presence => true , :on => :create_new
+	validates :subject, :presence => true , :on => :create_new
+	validates :topics, :presence => true , :on => :create_new
 	validates :message, :presence => true
 	validates_format_of :to_email,:with => Devise::email_regexp
 
@@ -15,7 +15,12 @@ class Mailbox < ActiveRecord::Base
 			Mailbox.select("mailboxes.id as mailid,*").where(:mail_to=>user_id,:parent_id=>0).joins("LEFT JOIN users u ON mailboxes.mail_from = u.id ").order("mailboxes.id desc")
 		end
 	end
-
+	def self.setStatusRead(parent_id)
+		Mailbox.where("parent_id = ? OR id = ?",parent_id,parent_id).update_all(:is_read=>true)
+	end
+	def self.inboxCount(user_id)
+		Mailbox.where(:mail_to=>user_id,:is_read=>false).count
+	end
 	private
 	def recipient_user
 		if self.to_email != nil
