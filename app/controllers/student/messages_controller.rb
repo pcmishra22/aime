0b33c897473b369254faa0dc7cf3e7488f27df43
Nilay <nilayanand@gmail.com>
2016-11-28 16:58:35 +0530

    Fix migration

--- app/controllers/student/messages_controller.rb
+++ app/controllers/student/messages_controller.rb
@@ -11,6 +11,11 @@ class Student::MessagesController < Student::AppController
     end
   end
 
+  def create
+    conversation = @conversation.messages.create({message: params[:message_text], sender_id: current_user.id})
+    render json: conversation
+  end
+
   private
   def set_conversation
     @conversation = Conversation.find(params[:conversation_id])
