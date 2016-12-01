82b532536f0645cce8cd8a38453fe7e767445cf9
Nilay <nilayanand@gmail.com>
2016-12-01 18:09:13 +0530

    Fix static pages route issue

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  get 'pages*' => 'pages#*', :format => false
 
   root to: 'visitors#index'
 
@@ -47,5 +46,8 @@ Rails.application.routes.draw do
       resources :messages
     end
   end
+
+  get 'pages*' => 'pages#*', :format => false
+
 
 end
